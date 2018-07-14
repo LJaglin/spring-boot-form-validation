d64054aec7b06839d91d05c14ba290fcfd4b271b
LJaglin <ljaglinski@o2.pl>
2018-07-14 16:44:34 +0200

    Add a simple web controller

---
+++ src/main/java/pl/ljaglin/springbootformvalidation/controllers/FormController.java
@@ -0,0 +1,15 @@
+package pl.ljaglin.springbootformvalidation.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FormController {
+
+    @RequestMapping(value="")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello!";
+    }
+}
