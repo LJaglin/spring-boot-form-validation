a5ea9a8c88565abe43db76270a7c6f0379ba9d01
LJaglin <ljaglinski@o2.pl>
2018-07-14 14:25:50 +0200

    Initial commit

---
+++ src/test/java/pl/ljaglin/springbootformvalidation/SpringBootFormValidationApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ljaglin.springbootformvalidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootFormValidationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
