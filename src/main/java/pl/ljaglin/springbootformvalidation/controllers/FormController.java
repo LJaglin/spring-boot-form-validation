87bf57f4908b7c0a09bb6f1e018f3fc6c055f50d
LJaglin <ljaglinski@o2.pl>
2018-07-15 13:41:55 +0200

    Add method that returns the form template

--- src/main/java/pl/ljaglin/springbootformvalidation/controllers/FormController.java
+++ src/main/java/pl/ljaglin/springbootformvalidation/controllers/FormController.java
@@ -1,6 +1,7 @@
 package pl.ljaglin.springbootformvalidation.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,5 +12,10 @@ public class FormController {
     @ResponseBody
     public String sayHello() {
         return "Hello!";
+    }
+
+    @GetMapping("form")
+    public String showForm() {
+        return "form";
     }
 }
