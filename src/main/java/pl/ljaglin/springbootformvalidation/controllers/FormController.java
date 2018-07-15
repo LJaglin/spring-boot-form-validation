e9d7ada294c312dd715c45c15c0e295b056ac648
LJaglin <ljaglinski@o2.pl>
2018-07-15 15:59:48 +0200

    Add method from WebMvcConfigurer Interface

--- src/main/java/pl/ljaglin/springbootformvalidation/controllers/FormController.java
+++ src/main/java/pl/ljaglin/springbootformvalidation/controllers/FormController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.ljaglin.springbootformvalidation.models.Person;
 
 import javax.validation.Valid;
 
 @Controller
-public class FormController {
+public class FormController implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/result").setViewName("result");
+    }
 
     @GetMapping("/")
     public String showForm(Person person) {
