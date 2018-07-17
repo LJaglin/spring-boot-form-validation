e24bd5b170be88f5c0a31a7a3e46afa92f4d82da
LJaglin <ljaglinski@o2.pl>
2018-07-17 19:47:41 +0200

    Use Integer instead of int

--- src/main/java/pl/ljaglin/springbootformvalidation/models/Person.java
+++ src/main/java/pl/ljaglin/springbootformvalidation/models/Person.java
@@ -16,7 +16,7 @@ public class Person {
 
     @NotNull
     @Min(18)
-    private int age;
+    private Integer age;
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
@@ -34,11 +34,11 @@ public class Person {
         return lastName;
     }
 
-    public void setAge() {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
